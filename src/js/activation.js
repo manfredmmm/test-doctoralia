36611e58b66107cb0096b1353e7c57f08e2dc3ab
manfred <manfred@adverway.com>
2017-02-22 09:20:40 +0100

    WIP slider

--- src/js/activation.js
+++ src/js/activation.js
@@ -1,27 +1,21 @@
 // --------------------------------------------------------------------------------
 // Activation First Class table
 // --------------------------------------------------------------------------------
-const domReady = function (callback) {
-  document.readyState === 'interactive' || document.readyState === 'complete' ?
-    callback() :
-    document.addEventListener('DOMContentLoaded', callback);
-};
-
-domReady(() => {
+document.addEventListener('DOMContentLoaded', () => {
+  // Toggle row class (checked) and change input checked property
   const toogleCheckbox = (event) => {
     const row = event.currentTarget;
     const value = row.getElementsByClassName('checkbox')[0];
-
     row.classList.toggle('checked');
     value.checked ? value.checked = false : value.checked = true;
   };
 
+  // Add listener for each table row
   const row = document.getElementsByClassName('js-enable__spec');
-
   for (let i = 0; i < row.length; i += 1) {
     row[i].addEventListener('click', (event) => {
       const notCheckbox = event.target.className !== 'checkbox';
-      
+      // Custom toggle on row click
       notCheckbox ? toogleCheckbox(event) : event.currentTarget.classList.toggle('checked');
     });
   }
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,10 +1,65 @@
-import $ from './jquery';
+//import $ from './jquery';
 
 // --------------------------------------------------------------------------------
 // Slider
 // --------------------------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', () => {
+  const timer = 2500;
+  let currentSlide = 0;
+  const nSlides = document.getElementsByClassName('js-slider__image').length;
 
-$(() => {
+  const getPreviousIndex = () => {
+    let previousIdx = currentSlide - 1;
+    if (previousIdx < 0) {
+      previousIdx = nSlides - 1;
+    }
+    return previousIdx;
+  };
+
+  const getSlide = n => document.getElementsByClassName('js-slider__image')[n];
+  const getText = n => document.getElementsByClassName('js-slide__item')[n];
+
+  const nextSlide = () => {
+    getSlide(getPreviousIndex()).classList.remove('previous');
+    getSlide(currentSlide).classList.remove('current');
+    getText(currentSlide).classList.remove('current');
+    currentSlide = (currentSlide + 1) % nSlides;
+  };
+
+  const showSlide = () => {
+    getSlide(getPreviousIndex()).classList.add('previous');
+    getSlide(currentSlide).classList.add('current');
+    getText(currentSlide).classList.add('current');
+  };
+
+  const showOverlay = () => {
+    getSlide(getPreviousIndex())
+      .getElementsByClassName('slider__overlay')[0].classList.remove('current');
+    getSlide(currentSlide)
+      .getElementsByClassName('slider__overlay')[0].classList.add('current');
+  };
+
+  const slideNeedsOverlay = () => {
+    const classes = getSlide(currentSlide).getElementsByClassName('slider__overlay')[0].classList;
+    // TODO find class on list
+    console.log(classes);
+    return !(classes.find(className => className === 'current').length > 0);
+  };
+
+  const carousel = () => {
+    if (slideNeedsOverlay()) {
+      showOverlay();
+    } else {
+      nextSlide();
+      showSlide();
+    }
+  };
+
+  setInterval(carousel, timer);
+  showSlide();
+});
+
+/*
   const timer = 2500;
   let currentSlide = 0;
   const nSlides = $('.js-slider__image').length;
@@ -51,4 +106,4 @@ $(() => {
 
   setInterval(carousel, timer);
   showSlide();
-});
+*/
