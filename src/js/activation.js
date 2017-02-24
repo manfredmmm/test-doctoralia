a8adb882a4e81c1ad6aec7bb6fec23be6b7d3bc2
manfred <manfred@adverway.com>
2017-02-24 14:06:49 +0100

    Added comments and activation controller fixes

--- src/js/activation.js
+++ src/js/activation.js
@@ -7,16 +7,34 @@ document.addEventListener('DOMContentLoaded', () => {
     const row = event.currentTarget;
     const value = row.getElementsByClassName('checkbox')[0];
     row.classList.toggle('checked');
-    value.checked ? value.checked = false : value.checked = true;
+    value.checked ?
+      value.checked = false :
+      value.checked = true;
   };
 
-  // Add listener for each table row
+  // Add listener for each row
   const row = document.getElementsByClassName('js-enable__spec');
   for (let i = 0; i < row.length; i += 1) {
     row[i].addEventListener('click', (event) => {
-      const notCheckbox = event.target.className !== 'checkbox';
       // Custom toggle on row click
-      notCheckbox ? toogleCheckbox(event) : event.currentTarget.classList.toggle('checked');
+      event.target.className !== 'checkbox' ?
+        toogleCheckbox(event) :
+        event.currentTarget.classList.toggle('checked');
     });
   }
+
+  // Add click listener to submit button
+  const classSubmit = document.getElementsByClassName('js-first-class')[0];
+  classSubmit.addEventListener('click', (event) => {
+    // Confirm if some input is checked
+    let activated = false;
+    for (let i = 0; i < row.length; i += 1) {
+      if (row[i].getElementsByClassName('checkbox')[0].checked) {
+        activated = true;
+      }
+    }
+    activated ?
+      console.log('Sending form') :
+      event.preventDefault();
+  });
 });
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,13 +1,16 @@
-//import $ from './jquery';
-
 // --------------------------------------------------------------------------------
 // Slider
 // --------------------------------------------------------------------------------
 document.addEventListener('DOMContentLoaded', () => {
+  // Variables:
+  // Each 2500ms we will see what we have to show;
+  // image (imageOverlay) or move
   const timer = 2500;
   let currentSlide = 0;
   const nSlides = document.getElementsByClassName('js-slider__image').length;
 
+  // Get previous index
+  // returns previous index
   const getPreviousIndex = () => {
     let previousIdx = currentSlide - 1;
     if (previousIdx < 0) {
@@ -16,9 +19,15 @@ document.addEventListener('DOMContentLoaded', () => {
     return previousIdx;
   };
 
+  // Get Slide (slider) or text (slides): Both are list item
+  // For slider: it contains two images (normal and overlayImage)
+  // For slides: it contains two spans
   const getSlide = n => document.getElementsByClassName('js-slider__image')[n];
   const getText = n => document.getElementsByClassName('js-slide__item')[n];
 
+  // Get next slide (li item)
+  // and remove the current/previous classes
+  // update currentSlide index (between 1 and 4)
   const nextSlide = () => {
     getSlide(getPreviousIndex()).classList.remove('previous');
     getSlide(currentSlide).classList.remove('current');
@@ -26,12 +35,16 @@ document.addEventListener('DOMContentLoaded', () => {
     currentSlide = (currentSlide + 1) % nSlides;
   };
 
+  // Show the current slide
+  // and add current/previous classes
   const showSlide = () => {
     getSlide(getPreviousIndex()).classList.add('previous');
     getSlide(currentSlide).classList.add('current');
     getText(currentSlide).classList.add('current');
   };
 
+  // Show overlay image for current slide
+  // and add/remove for current and previous overlay img
   const showOverlay = () => {
     getSlide(getPreviousIndex())
       .getElementsByClassName('slider__overlay')[0].classList.remove('current');
@@ -39,71 +52,24 @@ document.addEventListener('DOMContentLoaded', () => {
       .getElementsByClassName('slider__overlay')[0].classList.add('current');
   };
 
-  const slideNeedsOverlay = () => {
-    const classes = getSlide(currentSlide).getElementsByClassName('slider__overlay')[0].classList;
-    // TODO find class on list
-    console.log(classes);
-    return !(classes.find(className => className === 'current').length > 0);
-  };
+  // True if the current slide needs overlay
+  // = if the current slide doesn't have any img with current class (overlay)
+  const slideNeedsOverlay = () => !(getSlide(currentSlide).getElementsByClassName('current').length > 0);
 
+  // Carousel function called each 2500ms
   const carousel = () => {
     if (slideNeedsOverlay()) {
+      // Show overlay?
       showOverlay();
     } else {
+      // Normal behaviour
       nextSlide();
       showSlide();
     }
   };
 
   setInterval(carousel, timer);
+
+  // Initial state
   showSlide();
 });
-
-/*
-  const timer = 2500;
-  let currentSlide = 0;
-  const nSlides = $('.js-slider__image').length;
-
-  const getPreviousIndex = () => {
-    let previousIdx = currentSlide - 1;
-    if (previousIdx < 0) {
-      previousIdx = nSlides - 1;
-    }
-    return previousIdx;
-  };
-
-  const getSlide = n => $(`.js-slider__image:nth-child(${n + 1})`);
-  const getText = n => $(`.js-slide__item:nth-child(${n + 1})`);
-
-  const nextSlide = () => {
-    getSlide(getPreviousIndex()).removeClass('previous');
-    getSlide(currentSlide).removeClass('current');
-    getText(currentSlide).removeClass('current');
-    currentSlide = (currentSlide + 1) % nSlides;
-  };
-
-  const showSlide = () => {
-    getSlide(getPreviousIndex()).addClass('previous');
-    getSlide(currentSlide).addClass('current');
-    getText(currentSlide).addClass('current');
-  };
-
-  const showOverlay = () => {
-    getSlide(getPreviousIndex()).find('.slider__overlay').removeClass('current');
-    getSlide(currentSlide).find('.slider__overlay').addClass('current');
-  };
-
-  const slideNeedsOverlay = () => !getSlide(currentSlide).find('.slider__overlay').hasClass('current');
-
-  const carousel = () => {
-    if (slideNeedsOverlay()) {
-      showOverlay();
-    } else {
-      nextSlide();
-      showSlide();
-    }
-  };
-
-  setInterval(carousel, timer);
-  showSlide();
-*/
